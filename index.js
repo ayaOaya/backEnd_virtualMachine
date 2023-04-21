955564e7af1d5f6895241a85a472dcec7a4716b5
ayaOaya <alyaauaacn@gmail.com>
2023-04-21 15:04:31 +0200

    secnd commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const express = express()
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/virtual-workspace', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
+
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('A user has connected.');
+  
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected.');
+    });
+  
+    socket.on('message', (message) => {
+      console.log(`Received message: ${message}`);
+      io.emit('message', message);
+    });
+});
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
