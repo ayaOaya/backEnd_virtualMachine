75224f93b7e757fd7e6f5fa2cbbaa1e2ee936bae
ayaOaya <alyaauaacn@gmail.com>
2023-04-27 22:40:17 +0200

    establishing connection to mongo db using mongoose

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersRoute from "./routes/users.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = 5000;
 
+mongoose.connect('mongodb://localhost/users')
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
 app.use('/users', usersRoute)
 
 app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => {
     console.log("this is a text")
@@ -16,3 +23,4 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT, () => console.log(`Server runnig on port : http://localhost:${PORT}`));
+
